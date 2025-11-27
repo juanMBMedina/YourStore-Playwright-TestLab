1a4eedf5ba71dc1ad7168e18382c05d33085e5a5
Juan Miguel <juanmblancom@gmail.com>
2025-11-26 20:20:04 -0500

    Update local envronments

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: true,
+    headless: process.env.HEADLESS === "true",
     ignoreHTTPSErrors: true,
     viewport: null,
   },
